0dc33f65b130d04cdc1b244c11a66cb2af8b254a
ha-king <003king@gmail.com>
2023-05-19 19:21:26 +0000

    updating for TF

---
+++ tf/main.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudformation_stack" "eks-cluster" {
+  name = "eks-cluster-tf"
+  parameters = {
+    ParentVPCStack="vpc-2az"
+    ClusterName="EKS-DEV-TF"
+    EksVersion = "1.23"
+    KeyName = "eks-workers"
+    InstanceType = "t3.medium"
+  }
+  template_body = "${file("../eks-cfn.yaml")}"
+  capabilities = ["CAPABILITY_IAM"]
+}
